904a8c5657a065cefa8eb22a7f937973afdf0c4a
jflyer45 <jeremyfischer52@gmail.com>
2020-04-15 20:23:19 -0500

    Set up the basic welcome text and instructions

--- Final_Project_Sticker_JeremyF.py
+++ Final_Project_Sticker_JeremyF.py
@@ -4,11 +4,22 @@ from PIL import Image   # I will use this to modify the images.
 import openpyxl, requests         # I will use this to make the spread sheet.
 
 # Setting up the api
-department_url = 'https://collectionapi.metmuseum.org/public/collection/v1/departments'
-departments_data = requests.get(department_url).json()
+department_url = 'https://collectionapi.metmuseum.org/public/collection/v1/departments'  # This is the api url
+departments_data = requests.get(department_url).json()      # I store the data in a variable, using requests
 
+departments = []                                            # I create an empty string to append to
+# The loop below will get every department
+for entry in departments_data['departments']:               # For every entry (department) in the department data
+    departments.append(entry['displayName'])                # Append the value (department name)
 
-departments = []
-for dic in departments_data['departments']:
-    departments.append(dic['displayName'])
+print("Hello! Welcome to Jeremy's Art Sticker Project")     # Welcome text for user
+print("Select from one of the following departments, enter a number")       # Instructs users
+print()                                         # Blank space
+for x, department in enumerate(departments):    # I create a for loop to display all departments using enumerate
+    print(str(x + 1) + ".", department)         # I add 1 to x becuase it starts at 0, and I concatenate it with department.
+
+userinput = input("Enter department number: ")
+
+while userinput.isdecimal() is False:
+    userinput = input("Enter department number: ")
 
