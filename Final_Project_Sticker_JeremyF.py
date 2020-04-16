fffbad16b8c1899bdf267972032cae90dbaefdba
jflyer45 <jeremyfischer52@gmail.com>
2020-04-15 19:55:55 -0500

    I set up the department api, and begin work on getting the departments

--- Final_Project_Sticker_JeremyF.py
+++ Final_Project_Sticker_JeremyF.py
@@ -1,4 +1,14 @@
 # Jeremy Fisher 4/15/2020
 # These are my basic imports, I will probably need more later.
 from PIL import Image   # I will use this to modify the images.
-import openpyxl         # I will use this to make the spread sheet.
+import openpyxl, requests         # I will use this to make the spread sheet.
+
+# Setting up the api
+department_url = 'https://collectionapi.metmuseum.org/public/collection/v1/departments'
+departments_data = requests.get(department_url).json()
+
+
+departments = []
+for dic in departments_data['departments']:
+    departments.append(dic['displayName'])
+
