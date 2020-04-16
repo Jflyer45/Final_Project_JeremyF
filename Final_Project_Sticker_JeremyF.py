dabf078f0a5e3168ee183ae65bd5fda6f9b214c0
jflyer45 <jeremyfischer52@gmail.com>
2020-04-15 19:35:28 -0500

    I create the basic imports

---
+++ Final_Project_Sticker_JeremyF.py
@@ -0,0 +1,6 @@
+# Jeremy Fisher 4/15/2020
+# These are my basic imports, I will probably need more later.
+from PIL import Image   # I will use this to modify the images.
+import openpyxl         # I will use this to make the spread sheet.
+
+
