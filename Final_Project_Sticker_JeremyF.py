740477c414b6924a44a415cbe5f1b11f284ad7e7
jflyer45 <jeremyfischer52@gmail.com>
2020-04-15 21:19:40 -0500

    I test my loop and function to get ids with public_domain.

--- Final_Project_Sticker_JeremyF.py
+++ Final_Project_Sticker_JeremyF.py
@@ -3,6 +3,16 @@
 from PIL import Image   # I will use this to modify the images.
 import openpyxl, requests, random         # I will use this to make the spread sheet.
 
+
+def ispublicdomain(id):
+    collection_url = 'https://collectionapi.metmuseum.org/public/collection/v1/objects/' + str(id)
+    artwork_data = requests.get(collection_url).json()
+    if artwork_data['isPublicDomain'] is False:
+        return False
+    if artwork_data['isPublicDomain'] is True:
+        return True
+
+
 # Setting up the api
 department_url = 'https://collectionapi.metmuseum.org/public/collection/v1/departments'  # This is the api url
 departments_data = requests.get(department_url).json()      # I store the data in a variable, using requests
@@ -40,18 +50,11 @@ object_data = requests.get(objectsurl).json()
 total_objects = object_data['total']
 objectid_list = object_data['objectIDs']
 
-random_number = random.randint(0, total_objects)            # I use this varaible to select a random value from the objectid list!!!
-random_id = objectid_list[random_number]
-
-
-### I NEED TO MAKE A FUNCTION THAT CHECKS IF THERE IS A PUBLIC IMAGE AVAIBLE!!!
-
-
-
-def ispublicdomain(id):
-    collection_url = 'https://collectionapi.metmuseum.org/public/collection/v1/objects/' + id
-    artwork_data = requests.get(collection_url).json()
-    if artwork_data['isPublicDomain'] is False:
-        return False
-    if artwork_data['isPublicDomain'] is True:
-        return True
+while True:
+    random_number = random.randint(0, total_objects)  # I use this varaible to select a random value from the objectid list!!!
+    random_id = objectid_list[random_number]
+    if ispublicdomain(random_id) is False:
+        continue
+    else:
+        # Under this I will show the image and see if the user wants it
+        print("This is good" + str(random_id))
