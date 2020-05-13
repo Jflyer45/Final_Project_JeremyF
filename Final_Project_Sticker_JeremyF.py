c15acb0d697966781fabe5e164140217f470a0ec
jflyer45 <jeremyfischer52@gmail.com>
2020-05-13 10:14:09 -0500

    Fixed the random option to be more versitile, added more comments

--- Final_Project_Sticker_JeremyF.py
+++ Final_Project_Sticker_JeremyF.py
@@ -57,82 +57,81 @@ def main():                                                     # This is the ma
     department_url = 'https://collectionapi.metmuseum.org/public/collection/v1/departments'  # This is the api url
     try:                                                            # Prevents the crash
         departments_data = requests.get(department_url).json()      # I store the data in a variable, using requests
-    except:
-        print("Couldn't reach the server. Try checking your internet and re-run this program")
-        exit()
+    except:                                                         # If it does fail...
+        print("Couldn't reach the server. Try checking your internet and re-run this program")  # Explains failure
+        exit()                                                  # The program is stopped
 
     departments = []                                            # I create an empty string to append to
     # The loop below will get every department
     for entry in departments_data['departments']:               # For every entry (department) in the department data
         departments.append(entry['displayName'])                # Append the value (department name)
 
-    print("Hello! Welcome to Jeremy's Art Sticker Project")     # Welcome text for user
+    print("Hello! Welcome to Jeremy's Art Sticker Project")                      # Welcome text for user
     print("Select from one of the following departments, enter a number.")       # Instructs users
     print()                                         # Blank space
     for x, department in enumerate(departments):    # I create a for loop to display all departments using enumerate
         print(str(x + 1) + ".", department)         # I add 1 to x becuase it starts at 0, and I concatenate it with department.
 
-    while True:
-        userinput = digit_and_range_validation(1, len(departments))
-        collectionurl = 'https://collectionapi.metmuseum.org/public/collection/v1/objects?departmentIds='
-        if emptydepartmentcheck((collectionurl + str(userinput))) is True:
-            print("Unfortunally, at this time the department you have selected has no works of art, chose another one.")
-            continue
-        else:
-            break
+    # Loop tells if the department the users selects is empty or not
+    while True:                                                                                            # The loop
+        userinput = digit_and_range_validation(1, len(departments))                                        # Uses the validation funtion with the min being 1 and the max being the length of the list +1 (Becuase it starts at 1
+        collectionurl = 'https://collectionapi.metmuseum.org/public/collection/v1/objects?departmentIds='  # The url
+        if emptydepartmentcheck((collectionurl + str(userinput))) is True:                                 # If the department is empty
+            print("Unfortunally, at this time the department you have selected has no works of art, chose another one.")    # Explains to the user
+            continue                                        # Restarts the loop
+        else:                                               # Otherwise the department has data
+            break                                           # So the loop can be broke
 
-    objectsurl = collectionurl + str(userinput)
-    try:
-        object_data = requests.get(objectsurl).json()
-    except:
-        print("Couldn't reach the server. Try checking your internet and re-run this program")
-        exit()
-    chosen_department = departments[(userinput - 1)]
+    objectsurl = collectionurl + str(userinput)             # Combines the default url with the selected id
+    try:                                                    # Prevents the crash
+        object_data = requests.get(objectsurl).json()       # Finally gets the data for the last time
+    except:                                                 # If it fails
+        print("Couldn't reach the server. Try checking your internet and re-run this program")  # Explains
+        exit()                                              # Stops the program
 
-    total_objects = object_data['total']
-    objectid_list = object_data['objectIDs']
-
-    while True:
-        random_number = random.randint(1, total_objects)  # I use this varaible to select a random value from the objectid list!!!
-        random_id = objectid_list[random_number]
-        if ispublicdomain(random_id) is False:
-            continue
-        else:
+    total_objects = object_data['total']                    # Gets the number of artworks
+    objectid_list = object_data['objectIDs']                # Gets the list of object ids
+    print("Getting artwork...")                             # Tells user what's happening
+    print("This might take a few moments.")                 # Explains the wait
+    # This loop selects a random artwork from the chosen department
+    while True:                                             # Starts the loop
+        random_number = random.randint(1, total_objects)    # I use this varaible to select a random value from the objectid list!!!
+        random_id = objectid_list[random_number]            # Takes the random id from the list and stores it in a variable
+        if ispublicdomain(random_id) is False:              # Checks if the object is in the public domain
+            continue                                        # If it's not, the loop restarts
+        else:                                               # Otherwise if it's in the public domain
             # Under this I will show the image and see if the user wants it.
-            # I NEED TO FIND OUT HOW HE WANTS TO SHOW
-            potentartwork_url = 'https://collectionapi.metmuseum.org/public/collection/v1/objects/' + str(random_id)
-            try:
-                potentartwork_data = requests.get(potentartwork_url).json()
-            except:
-                print("Couldn't reach the server. Try checking your internet and re-run this program")
-                exit()
-            primaryImageSmall_url = potentartwork_data['primaryImageSmall']
-            urllib.request.urlretrieve(primaryImageSmall_url, "TempImage.jpg")
-            thumbnail = Image.open('TempImage.jpg')
-            thumbnail.show()
-            userinput = input('Do you want this image? (y/n): ').lower()
-            if userinput in ['y', 'yes']:
-                os.remove('TempImage.jpg')
-                break
-            else:
-                continue
+            potentartwork_url = 'https://collectionapi.metmuseum.org/public/collection/v1/objects/' + str(random_id)    # The url of the artwork the user might use
+            try:                                                                # Checks internet
+                potentartwork_data = requests.get(potentartwork_url).json()     # Gets the artwork data
+            except:                                                             # If it fails
+                print("Couldn't reach the server. Try checking your internet and re-run this program")  # Explains failure
+                exit()                                                              # Stops the program
+            primaryImageSmall_url = potentartwork_data['primaryImageSmall']         # Gets the thumbnail, easy download, version of the image
+            urllib.request.urlretrieve(primaryImageSmall_url, "TempImage.jpg")      # Saves the image as TempImage.jpg
+            thumbnail = Image.open('TempImage.jpg')                                 # opens the thumbnail
+            thumbnail.show()                                                        # Shows it to the user
+            userinput = input('Do you want this image? (y/n): ').lower()            # Asks if the user wants it and lowercases it
+            if userinput in ['y', 'yes']:                                           # If the input is the following
+                os.remove('TempImage.jpg')                                          # Removes the temp thumbnail
+                break                                                               # Breaks the loop
+            else:                                                                   # Others wise the answer is no, so
+                continue                                                            # Restart the process.
 
-    primaryImage_url = potentartwork_data['primaryImage']
-    try:
-        urllib.request.urlretrieve(primaryImage_url, "chosen_artwork_image.jpg")
-    except:
-        print("Couldn't reach the server. Try checking your internet and re-run this program")
-        exit()
-    image = Image.open('chosen_artwork_image.jpg')
+    primaryImage_url = potentartwork_data['primaryImage']                           # Now gets the fullsize image data
+    try:                                                                            # Tries to access the new image
+        urllib.request.urlretrieve(primaryImage_url, "chosen_artwork_image.jpg")    # Downloads the image
+    except:                                                                         # If it fails
+        print("Couldn't reach the server. Try checking your internet and re-run this program")  # Tells user why
+        exit()                                                                                  # Stops
+    image = Image.open('chosen_artwork_image.jpg')                  # Opens the image object
+    stickers = os.listdir('Stickers')                               # IMPORTANT: Makes a list of the stickers so it doesn't matter which stickers are added!!!
+    print()                                                         # Blank space
+    print("Chose a meme sticker or select random")                  # Instructs user
 
-    stickers = os.listdir('Stickers')
-
-    print()
-    print("Chose a meme sticker or select random")
-
-    for x, sticker in enumerate(stickers):
-        print(str(x + 1) + ".", sticker[:-4])
-    print("5. Random")
+    for x, sticker in enumerate(stickers):                          # Lists the sticker options
+        print(str(x + 1) + ".", sticker[:-4])                       # Formats the sting that will repeat
+    print(str(len(stickers) + 1) + ". Random")                      # Adds the
 
     userinput = digit_and_range_validation(1, (len(stickers) + 1))
 
